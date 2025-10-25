7c4e05525a86d89ef7e6480a062f3d2ce21e5f9d
flrs-94 <markus@flrs.it>
2025-10-25 17:13:46 +0200

    Add automatic nixos-rebuild after git pull

--- modules/gitops.nix
+++ modules/gitops.nix
@@ -9,7 +9,9 @@
       Type = "oneshot";
       WorkingDirectory = "/root/nixos-config";
       ExecStart = "${pkgs.git}/bin/git pull";
+      ExecStartPost = "${pkgs.nixos-rebuild}/bin/nixos-rebuild switch --flake /root/nixos-config#atom-c3758";
       Environment = "PATH=/run/current-system/sw/bin";
     };
   };
 }
+
