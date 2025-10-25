a8b703f1ccc0cffba24594e66df8862a545629cf
flrs-94 <markus@flrs.it>
2025-10-25 23:09:00 +0200

    Fix: patch top-level Makefile to skip systemd install target

--- overlays/qatlib.nix
+++ overlays/qatlib.nix
@@ -32,6 +32,180 @@ in {
       super.zlib.dev
     ];
 
+    preConfigure = ''
+      ./autogen.sh
+    '';
+
+    configureFlags = [ "--without-systemd" ];
+
+    preInstall = ''
+      substituteInPlace Makefile \
+        --replace 'install-systemdsystemunitDATA' ""
+    '';
+
+    installPhase = ''
+      make install
+    '';
+
+    meta = with super.lib; {
+      description = "Intel QuickAssist userspace library (qatlib)";
+      homepage = "https://github.com/intel/qatlib";
+      license = licenses.bsd3;
+      maintainers = [ maintainers.flores ];
+    };
+  };
+}
+
+/*
+self: super:
+
+let
+  qatlibSrc = super.fetchFromGitHub {
+    owner = "intel";
+    repo = "qatlib";
+    rev = "8575c0e2b8439d50f343476aed1e1f7bac60bf1d";
+    sha256 = "sha256-xAUS1ZEpFVSWg9/jp+oZMxzuEpCcKyBwjwHGGD/ETkg=";
+  };
+in {
+  qatlib = super.stdenv.mkDerivation {
+    pname = "qatlib";
+    version = "1.7.0";
+
+    src = qatlibSrc;
+
+    nativeBuildInputs = [
+      super.autoconf
+      super.automake
+      super.libtool
+      super.pkg-config
+      super.autoconf-archive
+      super.nasm
+    ];
+
+    buildInputs = [
+      super.openssl
+      super.udev
+      super.libtirpc
+      super.pciutils
+      super.numactl.dev
+      super.zlib.dev
+    ];
+
+    preConfigure = ''
+      ./autogen.sh
+    '';
+
+    configureFlags = [ "--without-systemd" ];
+
+    preInstall = ''
+      substituteInPlace quickassist/utilities/service/Makefile \
+        --replace 'install-systemdsystemunitDATA' ""
+    '';
+
+    installPhase = ''
+      make install
+    '';
+
+    meta = with super.lib; {
+      description = "Intel QuickAssist userspace library (qatlib)";
+      homepage = "https://github.com/intel/qatlib";
+      license = licenses.bsd3;
+      maintainers = [ maintainers.flores ];
+    };
+  };
+}
+
+/*self: super:
+
+let
+  qatlibSrc = super.fetchFromGitHub {
+    owner = "intel";
+    repo = "qatlib";
+    rev = "8575c0e2b8439d50f343476aed1e1f7bac60bf1d";
+    sha256 = "sha256-xAUS1ZEpFVSWg9/jp+oZMxzuEpCcKyBwjwHGGD/ETkg=";
+  };
+in {
+  qatlib = super.stdenv.mkDerivation {
+    pname = "qatlib";
+    version = "1.7.0";
+
+    src = qatlibSrc;
+
+    nativeBuildInputs = [
+      super.autoconf
+      super.automake
+      super.libtool
+      super.pkg-config
+      super.autoconf-archive
+      super.nasm
+    ];
+
+    buildInputs = [
+      super.openssl
+      super.udev
+      super.libtirpc
+      super.pciutils
+      super.numactl.dev
+      super.zlib.dev
+    ];
+
+    preConfigure = ''
+      ./autogen.sh
+      substituteInPlace quickassist/utilities/service/Makefile.am \
+        --replace 'install-systemdsystemunitDATA' ""
+    '';
+
+    configureFlags = [ "--without-systemd" ];
+
+    buildPhase = "make";
+
+    installPhase = ''
+      make install
+    '';
+
+    meta = with super.lib; {
+      description = "Intel QuickAssist userspace library (qatlib)";
+      homepage = "https://github.com/intel/qatlib";
+      license = licenses.bsd3;
+      maintainers = [ maintainers.flores ];
+    };
+  };
+}
+
+/*self: super:
+
+let
+  qatlibSrc = super.fetchFromGitHub {
+    owner = "intel";
+    repo = "qatlib";
+    rev = "8575c0e2b8439d50f343476aed1e1f7bac60bf1d";
+    sha256 = "sha256-xAUS1ZEpFVSWg9/jp+oZMxzuEpCcKyBwjwHGGD/ETkg=";
+  };
+in {
+  qatlib = super.stdenv.mkDerivation {
+    pname = "qatlib";
+    version = "1.7.0";
+
+    src = qatlibSrc;
+
+    nativeBuildInputs = [
+      super.autoconf
+      super.automake
+      super.libtool
+      super.pkg-config
+      super.autoconf-archive
+      super.nasm
+    ];
+
+    buildInputs = [
+      super.openssl
+      super.udev
+      super.libtirpc
+      super.pciutils
+      super.numactl.dev
+      super.zlib.dev
+    ];
+
     preConfigure = "./autogen.sh";
     configureFlags = [ "--without-systemd" ];
 
@@ -39,7 +213,6 @@ in {
     buildPhase = "make";
 
    installPhase = ''
-     substituteInPlace quickassist/utilities/service/Makefile --replace 'install-systemdsystemunitDATA' ""
      make install
    '';
     
@@ -51,3 +224,4 @@ in {
     };
   };
 }
+*/
