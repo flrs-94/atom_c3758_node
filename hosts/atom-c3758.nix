7d9ee6c764961059a5e10d14b2bb125ae9d778d4
flrs-94 <149864574+flrs-94@users.noreply.github.com>
2025-10-25 20:21:33 +0200

    Update Nix configuration for boot and user settings

--- hosts/atom-c3758.nix
+++ hosts/atom-c3758.nix
@@ -1,26 +1,57 @@
 { config, pkgs, ... }:
 
 {
-  # Hostname & Zeitzone
+# Boot und Kernel Parameter
+  system.stateVersion = "25.11";
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+  boot.kernelParams = [
+ 	"intel_iommu=on"
+	"iommu=pt"
+  # optional direktes VFIO-Binding (Vendor:Device IDs)
+  # "vfio-pci.ids=8086:10fb,8086:10ed"
+  ];
+  boot.initrd.kernelModules = [ 
+	"vfio"
+	"vfio_pci"
+	"vfio_iommu_type1"
+	"intel_qat"
+	"qat_c3xxx"
+  ];
+# modprobe Optionen
+  environment.etc."modprobe.d/99-qat.conf".text = ''
+  options qat_c3xxx max_vfs=4
+  '';
+# Hostname & Zeitzone
   networking.hostName = "atom-c3758";
   time.timeZone = "Europe/Berlin";
 
-  # Firewall: SSH + Cockpit
-
+# Firewall: SSH + Cockpit
   networking.firewall.allowedTCPPorts = [ 22 9090 ];
 
-  # Benutzer & Gruppen
-  users.users.flores = {
+# Benutzer & Gruppen
+  users.users.root.openssh.authorizedKeys.keys = [
+  "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQC6U/3CxfLatxlEro9deroGI9L23kkMBELlRFO9BdkyKVrKlj0rWKmmSvMAN92yRgaV5hjG3Y5wm>
+  ];
+  users.users.flrs = {
     isNormalUser = true;
     extraGroups = [ "wheel" "libvirtd" ];
+    openssh.authorizedKeys.keys = [
+      "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQC6U/3CxfLatxlEro9deroGI9L23kkMBELlRFO9BdkyKVrKlj0rWKmmSvMAN92yRgaV5hjG3>
+    ];
   };
 
-  # Flake-Unterstützung
+# Flake-Unterstützung
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
-  # Systempakete
+# Systempakete
   environment.systemPackages = with pkgs; [
-    pciutils
+	s-tui
+	qatlib
+	adf-tools
+	htop
+	powertop	
+	pciutils
 	ethtool
 	hwinfo
     usbutils
@@ -32,15 +63,13 @@
     openssl
   ];
 
-  # Dienste
+# Dienste
   services.openssh = {
       enable = true;
       settings = {
 	PermitRootLogin = "yes";
-	PasswordAuthentication = true;
       };
     };
-
   services.cockpit = {
   enable = true;
   settings = {
@@ -50,82 +79,20 @@
    };
  };
 
-
-  # Cockpit-Erweiterung für VM-Verwaltung
- /* services.cockpit.packages = with pkgs; [ cockpit-machines ]; */
-
-  # libvirt für Virtualisierung
+# libvirt für Virtualisierung
   virtualisation.libvirtd = {
     enable = true;
     qemu.package = pkgs.qemu_kvm;
   };
 
-  # Bootloader (UEFI)
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
-
-  # Root-Dateisystem
+# Root-Dateisystem
   fileSystems."/" = {
     device = "/dev/nvme1n1p2";
     fsType = "ext4";
   };
 
-  # GitOps-Modul einbinden
+# GitOps-Modul einbinden
   imports = [
     ./../modules/gitops.nix
   ];
-
-  # system.stateVersion setzen
-  system.stateVersion = "25.11";
 }
-
-
-/*{ config, pkgs, ... }:
-
-{
-  # Hostname und Zeitzone
-  networking.hostName = "atom-c3758";
-  time.timeZone = "Europe/Berlin";
-  
-  # Network & Firewall
-  networking.firewall.allowedTCPPorts = [ 22, 9090 ];
-
-  # User & Groups
-  users.users.flores.extraGroups = [ "libvirtd" ];
-
-  # Flake-Unterstützung
-  nix.settings.experimental-features = [ "nix-command" "flakes" ];
-
-  # Systempakete inkl. git + ssh
-  environment.systemPackages = with pkgs; [
-    git
-    openssh
-    cockpit
-    cockpit-machines
-    virt-manager
-  ];
-
-  # Services
-  virtualisation.libvirtd.enable = true;
-  services.openssh.enable = true;
-  services.cockpit.enable = true;
-
-  # Bootloader (UEFI)
-  boot.loader.systemd-boot.enable = true;
-  boot.loader.efi.canTouchEfiVariables = true;
-
-  # Root-Dateisystem
-  fileSystems."/" = {
-    device = "/dev/nvme1n1p2";
-    fsType = "ext4";
-  };
-
-  # GitOps-Modul einbinden
-  imports = [
-    ./../modules/gitops.nix
-  ];
-
-  # system.stateVersion setzen
-  system.stateVersion = "25.11";
-}
-*/
