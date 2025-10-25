3aabd254546d7830b8e6bea42212e30159300922
flrs-94 <markus@flrs.it>
2025-10-25 23:15:40 +0200

    Fix: patch Makefile.in to disable systemd install target before configure

--- overlays/qatlib.nix
+++ overlays/qatlib.nix
@@ -34,6 +34,62 @@ in {
 
     preConfigure = ''
       ./autogen.sh
+      substituteInPlace Makefile.in \
+        --replace 'install-systemdsystemunitDATA' ""
+    '';
+
+    configureFlags = [ "--without-systemd" ];
+
+    installPhase = ''
+      make install
+    '';
+
+    meta = with super.lib; {
+      description = "Intel QuickAssist userspace library (qatlib)";
+      homepage = "https://github.com/intel/qatlib";
+      license = licenses.bsd3;
+      maintainers = [ maintainers.flores ];
+    };
+  };
+}
+
+/*
+self: super:
+
+let
+  qatlibSrc = super.fetchFromGitHub {
+    owner = "intel";
+    repo = "qatlib";
+    rev = "8575c0e2b8439d50f343476aed1e1f7bac60bf1d";
+    sha256 = "sha256-xAUS1ZEpFVSWg9/jp+oZMxzuEpCcKyBwjwHGGD/ETkg=";
+  };
+in {
+  qatlib = super.stdenv.mkDerivation {
+    pname = "qatlib";
+    version = "1.7.0";
+
+    src = qatlibSrc;
+
+    nativeBuildInputs = [
+      super.autoconf
+      super.automake
+      super.libtool
+      super.pkg-config
+      super.autoconf-archive
+      super.nasm
+    ];
+
+    buildInputs = [
+      super.openssl
+      super.udev
+      super.libtirpc
+      super.pciutils
+      super.numactl.dev
+      super.zlib.dev
+    ];
+
+    preConfigure = ''
+      ./autogen.sh
     '';
 
     configureFlags = [ "--without-systemd" ];
