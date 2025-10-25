a011572b248c2d01a21b298a04731253be06badb
flrs-94 <markus@flrs.it>
2025-10-25 22:23:33 +0200

    Fix syntax: correct installPhase indentation in qatlib overlay

--- overlays/qatlib.nix
+++ overlays/qatlib.nix
@@ -5,6 +5,61 @@ let
     owner = "intel";
     repo = "qatlib";
     rev = "8575c0e2b8439d50f343476aed1e1f7bac60bf1d";
+    sha256 = "sha256-xAUS1ZEpFVSWg9/jp+oZMxzuEpCcKyBwjwHGGD/ETkg=";
+  };
+in {
+  qatlib = super.stdenv.mkDerivation {
+    pname = "qatlib";
+    version = "1.7.0";
+
+    src = qatlibSrc;
+
+    nativeBuildInputs = [
+      super.autoconf
+      super.automake
+      super.libtool
+      super.pkg-config
+      super.autoconf-archive
+      super.nasm
+    ];
+
+    buildInputs = [
+      super.openssl
+      super.udev
+      super.libtirpc
+      super.pciutils
+      super.numactl.dev
+      super.zlib.dev
+    ];
+
+    configurePhase = ''
+      ./autogen.sh
+      ./configure --prefix=$out --without-systemd
+    '';
+
+    buildPhase = "make";
+
+    installPhase = ''
+      substituteInPlace quickassist/utilities/service/Makefile --replace 'install-systemdsystemunitDATA' ''
+      make install
+    '';
+
+    meta = with super.lib; {
+      description = "Intel QuickAssist userspace library (qatlib)";
+      homepage = "https://github.com/intel/qatlib";
+      license = licenses.bsd3;
+      maintainers = [ maintainers.flores ];
+    };
+  };
+}
+
+/*self: super:
+
+let
+  qatlibSrc = super.fetchFromGitHub {
+    owner = "intel";
+    repo = "qatlib";
+    rev = "8575c0e2b8439d50f343476aed1e1f7bac60bf1d";
     sha256 = "sha256-xAUS1ZEpFVSWg9/jp+oZMxzuEpCcKyBwjwHGGD/ETkg="; 
   };
 in {
@@ -31,25 +86,28 @@ in {
       super.numactl.dev      
       super.zlib.dev
     ];
-
-    configureFlags = [
-      "--without-systemd"
-    ];
     
     configurePhase = ''
       ./autogen.sh
-      ./configure --prefix=$out
+      ./configure --prefix=$out --without-systemd
     '';
 
     buildPhase = "make";
 
-    installPhase = "make install";
+installPhase = ''
+  substituteInPlace quickassist/utilities/service/Makefile --replace 'install-systemdsystemunitDATA' ''
+  make install
+'';
 
     meta = with super.lib; {
       description = "Intel QuickAssist userspace library (qatlib)";
       homepage = "https://github.com/intel/qatlib";
       license = licenses.bsd3;
       maintainers = [ maintainers.flores ];
+
     };
+
   };
+
 }
+*/
