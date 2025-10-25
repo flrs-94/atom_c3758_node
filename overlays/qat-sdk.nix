d25c2aeb3d60bd78a247e6d1f3cbadfdad64221e
flrs-94 <markus@flrs.it>
2025-10-26 01:06:25 +0200

    Fix QAT SDK overlay: use systemd.dev for libudev headers

--- overlays/qat-sdk.nix
+++ overlays/qat-sdk.nix
@@ -13,18 +13,28 @@ in {
     src = qatlibSrc;
 
     nativeBuildInputs = with super; [ autoconf automake libtool pkg-config ];
-    buildInputs = with super; [ openssl zlib libudev ];
+    buildInputs = [
+      super.openssl
+      super.udev
+      super.libtirpc
+      super.pciutils
+      super.numactl.dev
+      super.zlib.dev
+    ];
+
 
     makeFlags = [ "KERNEL_SOURCE=${super.linux.dev}/lib/modules/${super.linux.modDirVersion}/build" ];
 
-    installPhase = ''
-      mkdir -p $out/bin
-      cp -v build/*/adf_ctl $out/bin/
-      cp -v build/*/qat_service $out/bin/ || true
-      mkdir -p $out/lib/modules
-      cp -v build/*/*.ko $out/lib/modules/
-    '';
+  installPhase = ''
+    mkdir -p $/out/bin
+    find . -type f -name adf_ctl -exec cp -v {} $/out/bin/ \;
+    find . -type f -name qat_service -exec cp -v {} $/out/bin/ \; || true
+    mkdir -p $/out/lib/modules
+    find . -type f -name '*.ko' -exec cp -v {} $/out/lib/modules/ \;
+  '';
 
+
+  
     meta = with super.lib; {
       description = "Intel QuickAssist Technology SDK with kernel modules and tools";
       homepage = "https://github.com/intel/qatlib";
@@ -34,43 +44,3 @@ in {
   };
 }
 
-/***
-self: super:
-
-let
-  qatlibSrc = super.fetchFromGitHub {
-    owner = "intel";
-    repo = "qatlib";
-    rev = "v24.02.0"; # oder HEAD für aktuellste Version
-    sha256 = "sha256-PLACEHOLDER"; # mit nix-prefetch-git oder nix-prefetch-url ermitteln
-  };
-in {
-  qat-sdk = super.stdenv.mkDerivation rec {
-    pname = "qat-sdk";
-    version = "24.02.0";
-
-    src = qatlibSrc;
-
-    nativeBuildInputs = with super; [ autoconf automake libtool pkg-config ];
-
-    buildInputs = with super; [ openssl zlib libudev ];
-
-    makeFlags = [ "KERNEL_SOURCE=${super.linux.dev}/lib/modules/${super.linux.modDirVersion}/build" ];
-
-    installPhase = ''
-      mkdir -p $out/bin
-      cp -v build/*/adf_ctl $out/bin/
- #    cp -v build/*/qat_service $out/bin/ || true
-  #   mkdir -p $out/lib/modules
-   #  cp -v build/*/*.ko $out/lib/modules/
-   # '';
-/*
-    meta = with super.lib; {
-      description = "Intel QuickAssist Technology SDK with kernel modules and tools";
-      homepage = "https://github.com/intel/qatlib";
-      license = licenses.bsd3;
-      platforms = platforms.linux;
-    };
-  };
-}
-***/
