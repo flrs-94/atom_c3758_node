d95cdd5862ea5994c2cd23aa032d1fcfc8664c32
flrs-94 <markus@flrs.it>
2025-10-27 09:35:16 +0100

    Fix rebuild.sh: Add experimental features flags

--- rebuild.sh
+++ rebuild.sh
@@ -3,7 +3,7 @@ cd /root/nixos-config
 git pull
 
 # Build system to a known path
-nix build .#nixosConfigurations.atom-c3758.config.system.build.toplevel --out-link /root/nixos-config/result-system
+nix --extra-experimental-features 'nix-command flakes' build .#nixosConfigurations.atom-c3758.config.system.build.toplevel --out-link /root/nixos-config/result-system
 
 # Activate directly, bypassing nixos-rebuild and systemd-run
 /root/nixos-config/result-system/bin/switch-to-configuration switch
